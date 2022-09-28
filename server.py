532115ec405b69d2870fcd18a3ea5357fafc6ef9
Rashmitha <30rashmitha@gmail.com>
2022-09-29 01:17:05 +0530

    update with face detecting rectangles

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from cgitb import grey
 import io
 import socket
 import struct
@@ -11,6 +12,19 @@ server_socket = socket.socket()
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((config('IP_ADDRESS'), int(config('PORT'))))  # ADD IP HERE
 server_socket.listen(1)
+path=config('PATH')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+
+def draw_found_faces(detected, image, color: tuple):
+    for (x, y, width, height) in detected:
+        cv2.rectangle(
+            image,
+            (x, y),
+            (x + width, y + height),
+            color,
+            thickness=2
+        )
 
 # Accept a single connection and make a file-like object out of it
 connection = server_socket.accept()[0].makefile('rb')
@@ -30,15 +44,21 @@ try:
         # processing on it
         image_stream.seek(0)
         image = Image.open(image_stream)
+        #faceCascade=cv2.CascadeClassifier(path)
         im = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)
         
-        if img is None:
-            cv2.imshow('Video',im)
-        else:
-            img.set_data(image)
-
-        pl.pause(0.0001)
-        pl.draw()
+        gray=cv2.cvtColor(numpy.array(image),cv2.COLOR_BGR2GRAY)
+        faces=face_cascade.detectMultiScale(gray,1.1,4)
+        profiles = profile_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(im, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        for (x, y, w, h) in profiles:
+            cv2.rectangle(im, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        print("face",faces)
+        print("profile",profiles)
+        #cv2.imshow('Video',im)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+           break
 
         print('Image is %dx%d' % image.size)
         image.verify()
