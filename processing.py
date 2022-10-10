caddf1e42d3bb46f18104c7b8b12fd56badeb574
Vighnesh2603 <Vighup2603@gmail.com>
2022-10-11 03:32:33 +0530

    corrcet face detection

--- processing.py
+++ processing.py
@@ -17,7 +17,7 @@ from glob import glob
 import face_alignment
 
 
-fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False, device='cuda:{}'.format(0))
+fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False, face_detector='blazeface', device='cuda:{}'.format(0))
 
 template = 'ffmpeg -loglevel panic -y -i {} -strict -2 {}'
 # template2 = 'ffmpeg -hide_banner -loglevel panic -threads 1 -y -i {} -async 1 -ac 1 -vn -acodec pcm_s16le -ar 16000 {}'
@@ -27,23 +27,15 @@ def process_video_file():
     frames = []
     while 1:
         still_reading, frame = video_stream.read()
-        preds = fa.get_landmarks(frame)
-        #print(preds)
-        # if preds != None:
-        #     #print(enumerate(preds))
-        #     for _, f in enumerate(preds):
-        #            x1, y1, x2, y2 = f
-        #            print(x1,x2,y1,y2)
-        #     x1,y1=preds[0][0]
-        #     x2,y2=preds[0][-1]
-        # else:
-        #     x1 = 0
-        #     x2 = 0
-        #     y1 = 0
-        #     y2 = 0
-        # cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
-        for x,y in preds[0]:
-            cv2.circle(frame,(int(x), int(y)) , radius=0, color=(0, 0, 255), thickness=-1)
+        preds = fa.get_landmarks(frame,return_bboxes=True)
+        if preds != (None, None, None):
+            cords=[]
+            print(preds)
+            for i in enumerate(preds[2][0]):
+                _ , x = i
+                cords.append(x)
+            x1, y1, x2, y2, _= cords
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('p'):
                 break
